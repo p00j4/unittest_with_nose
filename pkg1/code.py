ae2565ebf4158ce5edfb9bd6a5b9b407d2f6f99c
Pooja Shah <pooja@moengage.com>
2017-09-07 12:09:41 +0530

    intro: why unit test

--- pkg1/code.py
+++ pkg1/code.py
@@ -1,9 +1,17 @@
 import unittest
 
 class Calculator(object):
-
+    # 1st commit
     def add(self, x, y):
-        if type(x) != int or type(y) != int:
+        pass
+
+    # Improvisation 1
+    def add(self, x, y):
+        return x+y
+
+    # Improvisation 2
+    def add(self, x, y):
+        if not (isinstance(x, int)) or not(isinstance(y, int)):
             return "invalid parameters, only integers are accepted"
         return x+y
 
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,15 +3,31 @@ from pkg1.code import Calculator
 
 class TestCalculator(unittest.TestCase):
 
-    def test_add_method(self):
+    # 1st commit
+    def test_add_method_1(self):
+        calc = Calculator()
+        """ check addition for integer user inputs """
+        result = calc.add(2, 2)
+        self.assertEqual(4, result)
+
+    # test cover 2
+    def test_add_method_2(self):
         calc = Calculator()
         """ check addition for integer user inpouts """
         result = calc.add(2,2)
         self.assertEqual(4, result)
 
-        """ check addition for varied user inputs """
+        """ check addition for various user inputs """
         result = calc.add(2,'b')
         self.assertEqual('2b', result)
+
+    # test cover 3
+    def test_add_method_2(self):
+        calc = Calculator()
+        """ check addition for various user inputs """
+        result = calc.add(2, 'b')
+        self.assertEqual('invalid parameters, only integers are accepted', result)
+
 
 if __name__ == '__main__':
     unittest.main()
